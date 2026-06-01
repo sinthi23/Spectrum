4ca4ad534f9515432d7170cb1cf01f0892606e6b
sinthi23 <tanhaislamsinthi2003@gmail.com>
2026-06-01 12:06:02 +0600

    master page added

---
+++ Site.Master.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Web.UI;
+
+namespace SpectrumWebForms
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
